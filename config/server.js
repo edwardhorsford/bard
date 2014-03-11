87919c8a758cd89519b1a0a1db32c472ba33be8a
CJ Ponti <cjponti@gmail.com>
2014-03-11 09:06:09 +0000

    Project restructuring

--- config/server.js
+++ config/server.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var path = require('path');
+var engines = require('consolidate');
 var routes = require('./routes');
 var EXPRESS_PORT = 3000;
 
 var server = express();
 
 server.configure(function() {
-	server.set('port',EXPRESS_PORT);
-	server.set('view engine', 'jade');
-	server.use(express.static(__dirname + '../views'));
-	server.use(server.router);
+	server.set('port',EXPRESS_PORT)
+		.set('view engine', 'jade')
+		.set('views', path.join(__dirname, '../views'))
+		.engine('jade', engines.jade)
+		.use(express.static(path.join(__dirname,'../public')))
+		.use(server.router);
 });
+
+routes(server);
 
 server.listen(EXPRESS_PORT, function() {
 	console.log('Server listening on port ' + EXPRESS_PORT);
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,50 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+var markdown = require('gulp-markdown');
+var jshint = require('gulp-jshint');
+
+var files = {
+	js: [ './assets/js/**/*.js' ],
+	styl: ['./assets/stylus/**/*.styl' ],
+	jade: ['./views/**/*.jade' ],
+	notes: [ './notes/**/*.md' ]
+};
+
+gulp.task('watch', function() {
+	gulp.watch(files.js, [ 'client-js' ]);
+	gulp.watch(files.notes, [ 'notes' ]);
+	gulp.watch(files.jade, [ 'jade' ]);
+	gulp.watch(files.styl, [ 'stylus' ]);
+});
+
+gulp.task('client-js', function() {
+	gulp.src(files.js)
+		.pipe(jshint())
+		.pipe(gulp.dest('public/scripts'))
+});
+
+gulp.task('stylus', function() {
+	gulp.src(files.styl)
+		.pipe(stylus())
+		.pipe(gulp.dest('public/styles'))
+});
+
+gulp.task('notes', function() {
+	gulp.src(files.notes)
+        .pipe(markdown())
+        .pipe(gulp.dest('views/notes'));
+});
 
 gulp.task('jade', function() {
 	console.log('Put code for converting jade to html files here');
 });
 
+gulp.task('server', function() {
+	nodemon({script: './config/server.js'})
+		.on('restart', [ 'jade' ]);
+});
+
 // Default task
-gulp.task('default', [ 'jade' ]);
+gulp.task('default', [ 'server', 'watch' ]);
