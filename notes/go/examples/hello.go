079600cbf97f2ff2bd7d78f87cc8baa14ad2e0bd
CJ Ponti <cjponti@gmail.com>
2014-06-28 15:51:29 +0100

    Add examples to hello.go

--- notes/go/examples/hello.go
+++ notes/go/examples/hello.go
@@ -1,13 +1,23 @@
 package main
 
+/* Import packages */
 import (
 	"fmt"
 	"os"
 	"strings"
 )
 
+/* Declare a variable */
+var who string = "World"
+
 /*  Declare a constant */
 const name = "Meerkat"
+
+/* Declare an array */
+var array = []int{1, 2, 3, 4}
+
+/* Define a slice */
+var slice = array[0:2]
 
 /* Declare a list of constants */
 const (
@@ -16,26 +26,24 @@ const (
 	city     = "The Big Apple"
 )
 
+/* Declare a function */
 func add_em(x int, y int) int {
 	return x + y
 }
 
 func main() {
-	who := "World!"
+	/* Declare a variable within a function */
+	variable_in_function := "I'm in a function!"
 
-	/* Declare an array */
-	var array = []int{1, 2, 3, 4}
-
-	/* Ternary Operator */
-	num := "cats"
-
+	/* Conditional */
 	if len(os.Args) > 1 {
 		/* os.Args[0] is "hello" or "hello.exe" */
 		who = strings.Join(os.Args[1:], " ")
 	}
 
-	fmt.Println(num)
-	fmt.Println(array[1:])
-	fmt.Println(add_em(10, 15))
-	fmt.Println("Hello", who+"!")
+	fmt.Println("Array:", array)
+	fmt.Println("Slice:", slice)
+	fmt.Println("Varible In Function:", variable_in_function)
+	fmt.Println("Function Return Value:", add_em(10, 15))
+	fmt.Println("Name from command line:", "Hello", who+"!")
 }
