d448b47820cd8e2420bf65c8645a0a4f3446e8f8
CJ Ponti <cjponti@gmail.com>
2014-06-29 21:02:33 +0100

    Remove gorilla_handler from web_server.go

--- notes/go/examples/web_server.go
+++ notes/go/examples/web_server.go
@@ -9,15 +9,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func gorilla_handler(rw http.ResponseWriter, req *http.Request) {
-	val := req.URL.Path[2:]
-	if val == "yes" {
-		fmt.Fprintf(rw, "So you like gorillas, huh?")
-	} else if val == "no" {
-		fmt.Fprintf(rw, "Why don't you like gorillas, huh?")
-	}
-}
-
 func main() {
 	http.HandleFunc("/gorillas", gorilla_handler)
 	http.HandleFunc("/", handler)
